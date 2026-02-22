ff3db8eb2354ffd6f8f63166e0ac8cb189d759d7
AkhilRaparthi <raparthiakhil55@gmail.com>
2026-02-22 18:10:29 +0530

    ec2-update

---
+++ Ec2/ec2.tf
@@ -0,0 +1,61 @@
+
+resource "aws_instance" "test" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  tags = {
+    Name = "Devops"
+  }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc
+
+  tags = {
+    Name = "allow_tls"
+  }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+#Creating iam role
+resource "aws_iam_role" "ec2_role" {
+  name = "devops-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+#attaching policy to role
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+#creating instance profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "EC2-S3-Access"
+  role = aws_iam_role.ec2_role.name
+}
---
+++ Ec2/variables.tf
@@ -0,0 +1,3 @@
+variable "region" {default = "us-east-1"}
+variable "accountid" { default = "192481307176"}
+variable "vpc" { default = "vpc-0f4ae958d7b87114a" }
